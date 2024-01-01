26211643c5bc47436d4ac71ee8b17f1d2ac084e9
youthOfMe <204069080@qq.com>
2024-01-01 12:26:00 +0800

    配置craco 配置别名 配置less 配置样式重置 配置基本架构路由

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,24 @@
 const path = require('path')
+const CracoLessPlugin = require('craco-less')
+
 const resolve = pathname => path.resolve(__dirname, pathname)
 
 module.exports = {
+    // less
+    plugins: [
+        {
+            plugin: CracoLessPlugin,
+            // options: {
+            //     lessLoaderOptions: {
+            //         lessOptions: {
+            //             modifyVars: { '@primary-color': '#1DA57A' },
+            //             javascriptEnabled: true,
+            //         }
+            //     }
+            // }
+        }
+    ],
+    // webpack
     webpack: {
         alias: {
             "@": resolve("src"), // 需要写绝对路径哦
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom'
 import App from './App';
+
+import './assets/css/index.less'
+import 'normalize.css'
 
 // @ => src: webpack
 // 问题: react脚手架隐藏webpack
@@ -9,8 +13,12 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Suspense fallback='loading'>
+            <HashRouter>
+                <App />
+            </HashRouter>
+        </Suspense>
+    </React.StrictMode>
 );
 
