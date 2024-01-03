4f36b13b751c97ec774cfe9d166cb7d09f94a2ab
youthOfMe <204069080@qq.com>
2024-01-03 14:36:47 +0800

    增加顶部通栏的搜索组件

--- src/components/app-header/c-cpns/header-center/index.jsx
+++ src/components/app-header/c-cpns/header-center/index.jsx
@@ -1,10 +1,16 @@
 import React, { memo } from 'react'
 import { CenterWrapper } from './style'
+import IconSearchBar from '@/assets/svg/icon_search_bar'
 
 const HeaderCenter = memo(() => {
     return (
         <CenterWrapper>
-            center
+            <div className="search-bar">
+                <div className="text">搜索房源和体验</div>
+                <div className="icon">
+                    <IconSearchBar/>
+                </div>
+            </div>
         </CenterWrapper>
     )
 })
