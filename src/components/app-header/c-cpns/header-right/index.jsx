18607234cff6131758928752af8934f9aaf17129
youthOfMe <204069080@qq.com>
2024-01-03 14:21:56 +0800

    增加顶部通栏右侧的功能区域

--- src/components/app-header/c-cpns/header-right/index.jsx
+++ src/components/app-header/c-cpns/header-right/index.jsx
@@ -1,10 +1,24 @@
 import React, { memo } from 'react'
 import { RightWrapper } from './style'
+import IconGlobal from '@/assets/svg/icon_global'
+import IconMenu from '@/assets/svg/icon_menu'
+import IconAvatar from '@/assets/svg/icon.avatar'
 
 const HeaderRight = memo(() => {
     return (
         <RightWrapper>
-            right
+            <div className="btns">
+                <span className="btn">登录</span>
+                <span className="btn">注册</span>
+                <span className="btn">
+                    <IconGlobal></IconGlobal>
+                </span>
+            </div>
+
+            <div className="profile">
+                <IconMenu/>
+                <IconAvatar/>
+            </div>
         </RightWrapper>
     )
 })
--- src/components/app-header/c-cpns/header-right/style.js
+++ src/components/app-header/c-cpns/header-right/style.js
@@ -1,8 +1,55 @@
 import styled from "styled-components"
 
+const boxShadow = `        
+    transition: box-shadow 1s ease;
+    &:hover {
+        box-shadow: 0 2px 4px rgb(0, 0, 0, .18);
+    }
+`
+
 export const RightWrapper = styled.div`
     flex: 1;
     display: flex;
     justify-content: flex-end;
     align-items: center;
+
+    color: ${props => props.theme.text.primaryColor};
+    font-weight: 600;
+
+    .btns {
+        display: flex;
+        box-sizing: content-box;
+
+        .btn {
+            height: 18px;
+            line-height: 18px;
+            padding: 12px 15px;
+            border-radius: 22px;
+            cursor: pointer;
+            box-sizing: content-box;
+
+            &:hover {
+                background-color: #f5f5f5;
+            }
+        }
+    }
+
+    .profile {
+        position: relative;
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        width: 77px;
+        height: 42px;
+        margin-right: 24px;
+        box-sizing: border-box;
+        border: 1px solid #ccc;
+        border-radius: 25px;
+        background-color: #fff;
+        color: ${props => props.theme.text.primaryColor};
+        cursor: pointer;
+
+        /* 混入 */
+        ${props => props.theme.mixin.boxShadow}
+    }
 `
