9726da407652d766b529989f0d3615609dc0b811
youthOfMe <204069080@qq.com>
2024-01-05 20:53:08 +0800

    首页轮播图封装完毕

---
+++ src/views/home/c-cpns/home-banner/index.jsx
@@ -0,0 +1,10 @@
+import React, { memo } from 'react'
+import { BannerWrapper } from './style'
+
+const HomeBanner = memo(() => {
+    return (
+        <BannerWrapper></BannerWrapper>
+    )
+})
+
+export default HomeBanner
