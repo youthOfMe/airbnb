eef89ee97eed30acb47684418706374c34f874c7
youthOfMe <204069080@qq.com>
2024-01-19 22:45:53 +0800

    封装全部页面-顶部筛选栏

--- src/views/entire/c-cpns/entire-filter/index.jsx
+++ src/views/entire/c-cpns/entire-filter/index.jsx
@@ -1,12 +1,39 @@
-import React, { Component } from 'react'
+import React, { memo, useState } from 'react'
 import { FilterWrapper } from './style'
+import filterData from '@/assets/data/filter_data.json'
+import classNames from 'classnames'
 
-export class index extends Component {
-    render() {
-        return (
-            <FilterWrapper></FilterWrapper>
-        )
+const index = memo(() => {
+    const [ selectItems, setSelectItems ] = useState([])
+
+    function itemClickHandle(item) {
+        const newItems = [...selectItems]
+        if(newItems.includes(item)) {
+            const itemIndex = newItems.findIndex(filterItem => filterItem === item)
+            newItems.splice(itemIndex, 1)
+        } else newItems.push(item)
+        setSelectItems(newItems)
     }
-}
+
+    return (
+        <FilterWrapper>
+            <div className="filter">
+                {
+                    filterData.map((item, index) => {
+                        return (
+                            <div
+                                className={classNames("item", { active: selectItems.includes(item) })}
+                                key={item}
+                                onClick={e => itemClickHandle(item)}
+                            >
+                                {item}
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </FilterWrapper>
+    )
+})
 
 export default index
