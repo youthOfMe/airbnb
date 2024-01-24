dee44e04c571ecf13d3daf77efdff2887fda22a8
youthOfMe <204069080@qq.com>
2024-01-24 23:30:51 +0800

    封装全部页面的页面切换组件

--- src/views/entire/c-cpns/entire-pagination/index.jsx
+++ src/views/entire/c-cpns/entire-pagination/index.jsx
@@ -1,12 +1,42 @@
-import React, { Component } from 'react'
+import { fetchRoomListAction } from '@/store/modules/entire/actionCreators'
+import React, { memo } from 'react'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
 import { PaginationWrapper } from './style'
+import Pagination from '@mui/material/Pagination'
 
-export class index extends Component {
-    render() {
-        return (
-          <PaginationWrapper></PaginationWrapper>
-        )
+const EntirePagination = memo(() => {
+    const { totalCount, currentPage, roomList } = useSelector(state => ({
+        totalCount: state.entire.totalCount,
+        currentPage: state.entire.currentPage,
+        roomList: state.entire.roomList
+    }), shallowEqual)
+    const totalPage = Math.ceil(totalCount / 20)
+    const startCount = currentPage * 20 + 1
+    const endCount = (currentPage + 1) * 20
+
+    // 事件处理逻辑
+    const dispatch = useDispatch()
+    function pageChangeHandle(event, pageCount) {
+        window.scrollTo(0, 0)
+        // 更新最新的页码: redux => currentPage
+        dispatch(fetchRoomListAction(pageCount - 1))
     }
-}
 
-export default index
+    return (
+        <PaginationWrapper>
+            {
+                // 使用双!防止展示0
+                !!roomList.length && (
+                    <div className="info">
+                        <Pagination count={totalPage} onChange={pageChangeHandle}></Pagination>
+                        <div className="desc">
+                            第 {startCount} - {endCount} 个房源, 共超过 {totalCount} 个
+                        </div>
+                    </div>
+                )
+            }
+        </PaginationWrapper>
+    )
+})
+
+export default EntirePagination
--- src/views/entire/c-cpns/entire-pagination/style.js
+++ src/views/entire/c-cpns/entire-pagination/style.js
@@ -2,4 +2,37 @@ import styled from "styled-components";
 
 
 
-export const PaginationWrapper = styled.div``
+export const PaginationWrapper = styled.div`
+    z-index: 9999;
+    display: flex;
+    justify-content: center;
+
+    .info {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        .MuiPaginationItem-page {
+            margin: 0 9px;
+            &:hover {
+                text-decoration: underline;
+            }
+
+        }
+
+        
+        .MuiPaginationItem-icon {
+                font-size: 24px;
+        }
+
+        .MuiPaginationItem-page.Mui-selected {
+            background-color: #222;
+            color: #fff;
+        }
+
+        .desc {
+            margin-top: 16px;
+            color: #222;
+        }
+    }
+`
